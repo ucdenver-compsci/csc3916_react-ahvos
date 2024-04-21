b4ca27cdffc3412473fa9faf66bea8c774085500
ahvo <ashley.vo1210@gmail.com>
2024-04-21 11:44:34 -0600

    testt

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -50,6 +50,7 @@ export function fetchMovie(movieId) {
     }
 }
 
+/*
 export function fetchMovies() {
     return dispatch => {
         return fetch(`${env.REACT_APP_API_URL}/movies?reviews=true`, {
@@ -70,3 +71,4 @@ export function fetchMovies() {
         }).catch((e) => console.log(e));
     }
 }
+*/
